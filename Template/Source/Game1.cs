6b89aaca06dd88734c5cf782a4600ef01f9b468a
Gabriel Enriquez <thegabmeister@protonmail.com>
2026-05-22 15:42:56 +0800

    Subclass Nez.Core

--- Template/Source/Game1.cs
+++ Template/Source/Game1.cs
@@ -1,52 +1,28 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using Nez;
 
 namespace Template
 {
-    public class Game1 : Game
+    public class Game1 : Core
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
-
         public Game1()
+            : base(width: 1280, height: 720, isFullScreen: false, windowTitle: "Template")
         {
-            _graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
-        }
 
-        protected override void LoadContent()
-        {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            var scene = Scene.CreateWithDefaultRenderer(Color.CornflowerBlue);
+            scene.SetDesignResolution(1280, 720, Scene.SceneResolutionPolicy.ShowAllPixelPerfect);
 
-            // TODO: use this.Content to load your game content here
-        }
+            var box = scene.CreateEntity("box", new Vector2(640, 360));
+            box.AddComponent(new PrototypeSpriteRenderer(96, 96))
+                .SetColor(Color.White);
 
-        protected override void Update(GameTime gameTime)
-        {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
-
-            base.Update(gameTime);
-        }
-
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
-
-            base.Draw(gameTime);
+            Scene = scene;
         }
     }
 }
